a37669a212bf942536020719e821b40b2c885bae
coronaisme <coronaisme@gmail.com>
2020-02-19 11:13:10 -0800

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,23 @@
 // Code CoordinatesButton Component Here
+import React, {Component} from 'react'
+
+export default class CoordinatesButton extends Component {
+
+
+  handleClick = (event) => {
+    // console.log(event.clientX, "x coord")
+    // console.log(event.clientY, "y coord")
+    let arr = []
+    arr.push(event.clientX)
+    arr.push(event.clientY)
+    // console.log(this.props.onReceiveCoordinates(arr))
+    this.props.onReceiveCoordinates(arr)
+   
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}></button>
+    )
+  }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,23 @@
 // Code DelayedButton Component Here
+import React, {Component} from 'react'
+
+export default class DelayedButton extends Component {
+
+  handleClickEvent = (event) => {
+    console.log(event, "event")
+    console.log(this.props)
+    console.log(event.target)
+    //cannot set the event to a const variable, 
+    //even though last lab said we could do either persist() or set to a variable
+    event.persist()
+    setTimeout(() => this.props.onDelayedClick(event), this.props.delay)
+    
+  }
+
+  render(){
+    // console.log(this.props)
+    return(
+      <button onClick={this.handleClickEvent}></button>
+    )
+  }
+}
